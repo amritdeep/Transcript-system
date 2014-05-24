8deafff536bace304e2e85aea5d31372f9e4b34d
amritdeep <amritdeepdhungana@hotmail.com>
2014-05-24 21:28:32 +0545

    create course model with name field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140524150520) do
+ActiveRecord::Schema.define(version: 20140524154156) do
+
+  create_table "courses", force: true do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "courses", ["user_id"], name: "index_courses_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
