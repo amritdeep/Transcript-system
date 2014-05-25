eaaf5f15c9f4133c5f2a16eaf95aeddb35b049c2
amritdeep <amritdeepdhungana@hotmail.com>
2014-05-25 10:51:22 +0545

    Change subject grade to float value

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,3 +1,19 @@
 class Subject < ActiveRecord::Base
   belongs_to :season
+
+
+  def grade_cal(grade)
+  	case grade
+  	when 75..100 then "A"
+  	when 60..75 then "A+"
+  	when 45..60 then "A-"
+  	when 30..45 then "B"
+  	when 15..30 then "B+"
+  	when 0..15 then "B-"
+  	else
+  		"ERROR"  	  			  		
+  	end  	
+  end
+
+
 end
--- db/migrate/20140524161114_create_subjects.rb
+++ db/migrate/20140524161114_create_subjects.rb
@@ -2,7 +2,7 @@ class CreateSubjects < ActiveRecord::Migration
   def change
     create_table :subjects do |t|
       t.string :name
-      t.string :grade
+      t.float :grade
       t.references :season, index: true
 
       t.timestamps
