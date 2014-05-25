b514a4c6b8e351fd67eb5aeaa525ecbadacb0a37
amritdeep <amritdeepdhungana@hotmail.com>
2014-05-25 11:45:06 +0545

    test for subject model to calculate grade

--- test/models/subject_test.rb
+++ test/models/subject_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class SubjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "should calculate te grade" do
+		@subject = Subject.create(name: "Computer", grade: 62.42)
+		assert_equal(@subject.grade_cal(@subject.grade), "A+")
+	end
 end
