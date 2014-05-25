f31813c830cef90695bf85bb9e94676e97e193f2
amritdeep <amritdeepdhungana@hotmail.com>
2014-05-25 11:38:58 +0545

    passed season test to have many subject

--- test/models/season_test.rb
+++ test/models/season_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class SeasonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "should have many subject" do
+		@season = Season.create(name: "Winter")
+		@subject1 = Subject.create(name: "Math")
+		@subject2 = Subject.create(name: "English")
+		@season.subjects = [@subject1, @subject2]
+		assert_includes(@season.subjects, @subject1)
+		assert_includes(@season.subjects, @subject2)
+	end
+
 end
